03e3fd1db1cfe4e1a0273eede6358931b6498f71
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-07-29 11:50:54 +0800

    自动提交: 周一 2024/07/29 at 11:50:54.47

--- 自动提交.bat
+++ 自动提交.bat
@@ -5,8 +5,8 @@ chcp 65001 >nul
 
 :: 设置你的仓库目录
 ::以下是set REPO_DIR="C:\Users\86166\Desktop\koa-1"的相对路径
-set REPO_DIR=%~dp0..\koa-1
-
+::set REPO_DIR=%~dp0..\koa-1
+set REPO_DIR="C:\Users\86166\Desktop\koa-1"
 :: 切换到仓库目录
 cd /d "%REPO_DIR%"
 
