ef448a33ba51f3c1bbc2278f2e4607acbdecda53
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-03 11:51:52 +0800

    自动提交: 周六 2024/08/03 at 11:51:52.30

--- 自动提交.bat
+++ 自动提交.bat
@@ -24,7 +24,7 @@ for /f "delims=" %%i in ('git status --porcelain') do (
        for /f "tokens=2,3,4 delims=/. " %%a in ("%date%") do (
         set DATE=%%a/%%b/%%c
         )
-        set TIME=%time:~0,8%
+        set TIME=%time:~0,5%
         set COMMIT_MSG=自动提交: %DATE% at %TIME%
 
         :: 提交更改
