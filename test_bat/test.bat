4f881939ffcfc773aa9459218329afc45400ad11
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-03 15:38:27 +0800

    自动提交: 周六 2024/08/03 at 15:38

--- test_bat/test.bat
+++ test_bat/test.bat
@@ -1,4 +1,5 @@
  @echo off
+ setlocal enabledelayedexpansion
  chcp 65001 >nul
  rem 测试
 
@@ -23,14 +24,22 @@ cd /d "%REPO_DIR%"
 
 for /f "delims=" %%i in ('git status ') do (
    echo -------------
-   echo %%i
+   set aa=%%i
+   set bb=!aa:~0,34!
+   echo !bb!
+   @REM 确保在 if 语句中使用双引号来包裹字符串 "Your branch is ahead of 'koa/main'"，以确保字符串完全匹配。
+    if "!bb!"=="Your branch is ahead of 'koa/main'" (
+        echo ok
+    ) else (
+        echo no
+    )
 )
  for /f "tokens=2,3,4 delims=/. " %%a in ("%date%") do (
-    set DATE=%%a/%%b/%%c
+   set DATE=%%a/%%b/%%c
 )
    set T=%time:~0,5%
-   set COMMIT_MSG=自动提交: %DATE% at %T%
-echo %T%
-endlocal
+   set COMMIT_MSG=自动提交: %DATE% at !T!
+ echo %T%
+ endlocal
 
  
