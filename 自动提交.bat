f4b34be6849324d1b4eecc004e6b65bb7bfe0e98
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-03 17:13:13 +0800

    自动提交: 周六 2024/08/03 at 17:13

--- 自动提交.bat
+++ 自动提交.bat
@@ -51,9 +51,9 @@ for /f "delims=" %%i in ('git status --porcelain') do (
    echo !bb!
    ::变量被双引号包裹了，那么要比较的值也要被双引号包裹，这样才能正确比较。
     if "!bb!"=="Your branch is ahead of 'koa/main'" (
-        echo 提交失败，等待5分钟后重试。
-        :: 等待1分钟
-        ping -n 300 localhost >nul
+        echo 自动提交失败，等待5分钟后自动重试。
+  
+        ping -n 30 localhost >nul
 
         echo 开始再次推送更改。
         git push
