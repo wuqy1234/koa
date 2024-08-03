ac3fdd149e4ab340c5b8fa88e56a2fe0306953f2
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-03 23:28:31 +0800

    自动提交: 周六 2024/08/03 at 23:28

--- tomorrow_auto_commit_github.bat
+++ tomorrow_auto_commit_github.bat
@@ -3,5 +3,6 @@ setlocal
 chcp 65001 >nul
 call c:\Users\86166\Desktop\koa-1\自动提交.bat
 schtasks /delete /tn "tomorrow_auto_commit_github" 
-del c:\Users\86166\Desktop\koa-1\tomorrow_auto_commit_github.bat /f
+
 endlocal
+del c:\Users\86166\Desktop\koa-1\tomorrow_auto_commit_github.bat /f
