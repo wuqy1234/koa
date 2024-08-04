3fe69bf9a10fe57a6b1fe104e6f7c594fbea2192
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-04 10:10:25 +0800

    自动提交: 周日 2024/08/04 at 10:10

--- 自动提交.bat
+++ 自动提交.bat
@@ -29,11 +29,11 @@ for /f "delims=" %%i in ('git status --porcelain') do (
             echo 自动提交失败,等待5分钟后自动重试。
             ping -n 300 localhost >nul
             echo 开始再次推送更改。
-                git push
+                @REM git push
         ) else (
                 echo 当前未连接任何 WiFi
                 echo 正在创建计划任务,等待明天9:30自动重试。
-                schtasks /create /tn "tomorrow_auto_commit_github" /tr "%~f0" /sc once /st 10:07 /f
+                schtasks /create /tn "tomorrow_auto_commit_github" /tr "%~f0" /sc once /st 10:11 /f
                 @REM schtasks /run /tn "tomorrow_auto_commit_github"
             )
     )
