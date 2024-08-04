bc89f837cf77e8a6856b1e367bde121cbadfb759
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-04 11:58:51 +0800

    自动提交: 周日 2024/08/04 at 11:58

--- 自动提交.bat
+++ 自动提交.bat
@@ -35,7 +35,7 @@ for /f "delims=" %%i in ('git status --porcelain') do (
                 git push
         ) else (
                 echo 当前未连接任何 WiFi
-                echo 正在创建计划任务,等待明天9:30自动重试。
+                echo 已创建计划任务,等待明天9:30自动重试。
                 schtasks /create /tn "!task_name!_tomorrow" /tr "%~f0" /sc daily /st 09:30 /f
             )
     )
