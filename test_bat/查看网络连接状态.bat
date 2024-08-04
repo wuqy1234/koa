83056b001c925720426ade10c298e96fa43c99e7
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-04 17:42:43 +0800

    自动提交: 周日 2024/08/04 at 17:42

--- test_bat/查看网络连接状态.bat
+++ test_bat/查看网络连接状态.bat
@@ -1,6 +1,6 @@
 @echo off
-setlocal
- chcp 65001 >nul
+setlocal enabledelayedexpansion
+chcp 65001 >nul
  
 :: 查询当前连接的 WiFi 名称
 for /f "tokens=5 delims=: " %%a in ('netsh wlan show interfaces ^| findstr "SSID"') do (
@@ -18,11 +18,8 @@ netsh wlan show interfaces >test_bat\wifi_info.txt
 
 netsh wlan show interfaces | findstr "SSID">test_bat\wifi_SSID.txt
 
-@REM git status | findstr "Your branch is ahead of*">test_bat\git_status.txt
+git status | findstr "Your branch is ahead of*">test_bat\git_status.txt
 @REM start /wait cmd /c "git push"
 @REM git push | findstr "fatal: unable to access*" > test_bat\git_push.txt
 
- for /f "delims=" %%i in ('git status') do (
- echo %%i > test_bat\git_push.txt
- )
 endlocal
