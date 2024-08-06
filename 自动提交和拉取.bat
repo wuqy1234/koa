33713e7130b8bc4808ba21781b0a59e758d82b65
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-06 10:35:55 +0800

    自动提交: 周二 2024/08/06 at 10:35

--- 自动提交和拉取.bat
+++ 自动提交和拉取.bat
@@ -27,8 +27,8 @@ for /f "delims=" %%i in ('git status --porcelain') do (
         )
         if defined SSID (
             echo 当前连接的 WiFi 名称为: %SSID%
-            echo 自动提交失败,每5分钟自动重试一次。
-            ping -n 300 localhost >nul
+            echo 自动提交失败,每5秒钟自动重试一次。
+            ping -n 5 localhost >nul
             echo 开始再次推送更改,直到成功。
             call "%~f0"
         ) else (
