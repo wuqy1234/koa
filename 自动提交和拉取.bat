24b950f90249b842c1860ab82a368230412d553e
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-07 18:37:31 +0800

    自动提交: 周三 2024/08/07 at 18:37

--- 自动提交和拉取.bat
+++ 自动提交和拉取.bat
@@ -13,34 +13,36 @@ for /f "delims=" %%i in ('git status --porcelain') do (
         set TIME=%time:~0,5%
         set COMMIT_MSG=自动提交: %DATE% at !TIME!
         git commit -m "!COMMIT_MSG!"
-        git push 
-        schtasks /delete /tn "!task_name!_tomorrow" /f
+        @REM git push 
+        @REM schtasks /delete /tn "!task_name!_tomorrow" /f
+        github
+
     )
 )
 
- for /f "delims=" %%i in ('git status') do (
-    set aa=%%i
-    set bb=!aa:~0,23!
-    if "!bb!"=="Your branch is ahead of" (
-        for /f "tokens=5 delims=: " %%a in ('netsh wlan show interfaces ^| findstr "SSID"') do (
-            set "SSID=%%a"
-        )
-        if defined SSID (
-            echo 当前连接的 WiFi 名称为: %SSID%
-            echo 自动提交失败,每5秒钟自动重试一次。
-            ping -n 5 localhost >nul
-            echo 开始再次推送更改,直到成功。
-            call "%~f0"
-        ) else (
-                echo 当前未连接任何 WiFi
-                echo 已创建计划任务,等待明天9:30自动重试。
-                schtasks /create /tn "!task_name!_tomorrow" /tr "%~f0" /sc daily /st 09:30 /f
-            )
-    ) 
-    if "!bb!"=="Your branch is up to da" (
-        echo 当前没有修改需要提交，正在从远端拉取最新更改。
-        git pull
-    )
-)
+@REM  for /f "delims=" %%i in ('git status') do (
+@REM     set aa=%%i
+@REM     set bb=!aa:~0,23!
+@REM     if "!bb!"=="Your branch is ahead of" (
+@REM         for /f "tokens=5 delims=: " %%a in ('netsh wlan show interfaces ^| findstr "SSID"') do (
+@REM             set "SSID=%%a"
+@REM         )
+@REM         if defined SSID (
+@REM             echo 当前连接的 WiFi 名称为: %SSID%
+@REM             echo 自动提交失败,每5秒钟自动重试一次。
+@REM             ping -n 5 localhost >nul
+@REM             echo 开始再次推送更改,直到成功。
+@REM             call "%~f0"
+@REM         ) else (
+@REM                 echo 当前未连接任何 WiFi
+@REM                 echo 已创建计划任务,等待明天9:30自动重试。
+@REM                 schtasks /create /tn "!task_name!_tomorrow" /tr "%~f0" /sc daily /st 09:30 /f
+@REM             )
+@REM     ) 
+@REM     if "!bb!"=="Your branch is up to da" (
+@REM         echo 当前没有修改需要提交，正在从远端拉取最新更改。
+@REM         git pull
+@REM     )
+@REM )
 
 endlocal
