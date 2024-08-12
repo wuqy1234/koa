2fed81eab38b028ff601fd272e7c22bac5b285e7
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-12 17:24:34 +0800

    自动提交: 周一 2024/08/12 at 17:24

--- 自动提交和拉取.bat
+++ 自动提交和拉取.bat
@@ -52,14 +52,14 @@ for /f "delims=" %%i in ('git status --porcelain') do (
         set TIME=%time:~0,5%
         set COMMIT_MSG=自动提交: %DATE% at !TIME!
         git commit -m "!COMMIT_MSG!"
-        github "C:\Users\86166\Desktop\koa-1"
+        github "%CD%"
     )
 )
  for /f "delims=" %%i in ('git status') do (
     set aa=%%i
     set bb=!aa:~0,23!
     if "!bb!"=="Your branch is ahead of" (
-       github "C:\Users\86166\Desktop\koa-1"
+       github "%CD%"
     )
 )
      
