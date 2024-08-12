37cf164b8a9b651a8a9d875fb3dcc5a8b6ebd7ed
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-08-12 17:33:26 +0800

    自动提交: 周一 2024/08/12 at 17:33

--- test_bat/获取根目录名.bat
+++ test_bat/获取根目录名.bat
@@ -5,12 +5,12 @@ chcp 65001 >nul
 
 :: 获取当前目录的名称
 for /f %%a in ("%CD%") do (set task_name=%%~nxa_auto_commit)
-
+echo "%CD%"    "%~dp0"
 echo !task_name!
 set task_name=!task_name!_tomorrow
 echo !task_name!
 
- schtasks /create /tn "!task_name!" /tr "%~f0" /sc daily /st 22:33 /f
+@REM  schtasks /create /tn "!task_name!" /tr "%~f0" /sc daily /st 22:33 /f
 
 
 endlocal
--- 自动提交和拉取.bat
+++ 自动提交和拉取.bat
@@ -4,48 +4,6 @@ chcp 65001 >nul
 set REPO_DIR=%~dp0
 cd /d "%REPO_DIR%"
 
-@REM for /f %%a in ("%CD%") do (set task_name=%%~nxa_auto_push)
-@REM schtasks /create /tn "!task_name!" /tr "%~f0" /sc daily /st 21:30 /f
-
-@REM for /f "delims=" %%i in ('git status --porcelain') do (
-@REM     if not "%%i" == "" (
-@REM         git add .
-@REM         set TIME=%time:~0,5%
-@REM         set COMMIT_MSG=自动提交: %DATE% at !TIME!
-@REM         git commit -m "!COMMIT_MSG!"
-@REM         @REM git push 
-@REM         @REM schtasks /delete /tn "!task_name!_tomorrow" /f
-@REM         github
-
-@REM     )
-@REM )
-
-@REM  for /f "delims=" %%i in ('git status') do (
-@REM     set aa=%%i
-@REM     set bb=!aa:~0,23!
-@REM     if "!bb!"=="Your branch is ahead of" (
-@REM         for /f "tokens=5 delims=: " %%a in ('netsh wlan show interfaces ^| findstr "SSID"') do (
-@REM             set "SSID=%%a"
-@REM         )
-@REM         if defined SSID (
-@REM             echo 当前连接的 WiFi 名称为: %SSID%
-@REM             echo 自动提交失败,每5秒钟自动重试一次。
-@REM             ping -n 5 localhost >nul
-@REM             echo 开始再次推送更改,直到成功。
-@REM             call "%~f0"
-@REM         ) else (
-@REM                 echo 当前未连接任何 WiFi
-@REM                 echo 已创建计划任务,等待明天9:30自动重试。
-@REM                 schtasks /create /tn "!task_name!_tomorrow" /tr "%~f0" /sc daily /st 09:30 /f
-@REM             )
-@REM     ) 
-@REM     if "!bb!"=="Your branch is up to da" (
-@REM         echo 当前没有修改需要提交，正在从远端拉取最新更改。
-@REM         git pull
-@REM     )
-@REM )
-
-
 for /f "delims=" %%i in ('git status --porcelain') do (
     if not "%%i" == "" (
         git add .
