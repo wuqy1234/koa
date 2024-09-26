b09b0e99dfb85632368f0f424d2d14b6ad4fde6e
wuqy1234 <156554960+wuqy1234@users.noreply.github.com>
2024-09-26 21:43:53 +0800

    更新 启动命令start.bat

--- test_bat/已经测试过了/启动命令start.bat
+++ test_bat/已经测试过了/启动命令start.bat
@@ -4,7 +4,7 @@
  chcp 65001 >nul
 
  start "" "C:\Users\Public\Desktop\酷我音乐.lnk"
- start "" "C:\Users\86166\Desktop\Visual Studio Code.lnk" %1
-
+@REM  start "" "C:\Users\86166\Desktop\Visual Studio Code.lnk" %1
+start "" "C:\Users\86166\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\MyKeymap.lnk"
 
 endlocal
